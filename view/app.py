7709d419a19a5389ccc6c9e360141896620481bc
Arthur <arthurpdesimone@gmail.com>
2021-02-04 18:57:44 -0300

    Documenting the project made so far

--- view/app.py
+++ view/app.py
@@ -1,6 +1,7 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
 import sys
+import win32gui, win32con
 
 class App(ShowBase):
     """Class to encapsulate the app and maximize window on Windows environment"""
@@ -11,13 +12,19 @@ class App(ShowBase):
         self.disable_mouse()
         """Maximizing windows"""
         props = WindowProperties()
-        props.set_size(1366, 768)
-        props.setFullscreen(True)
         props.setTitle('Aurora Python')
+
+
+
         self.win.requestProperties(props)
         self.setBackgroundColor(0, 0, 0)
         """ Configuration of camera and running """
         self.camera.setPos(4, -10, 2)
+
+        """ To maximize window under windows"""
+        self.hwnd = win32gui.GetForegroundWindow()
+        win32gui.PostMessage(self.hwnd, win32con.WM_SYSCOMMAND, win32con.SC_MAXIMIZE, 0)
+
         self.accept('escape', lambda: sys.exit())
 
 
