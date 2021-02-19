e152a32927997b15a6de7589089bd23e441adb0f
Arthur <arthurpdesimone@gmail.com>
2021-02-19 15:58:06 -0300

    Creating Footing class

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
+autoclass_content = 'both'
 
 # -- Options for HTML output -------------------------------------------------
 
--- model/footing/Footing.py
+++ model/footing/Footing.py
@@ -1,6 +1,12 @@
 class Footing():
     """Class to define a footing"""
     def __init__(self,x,y):
+        """Initializer
+        :param x: The center X position
+        :type x: float
+        :param y: The center Y position
+        :type y: float
+        """
         self.x = x
         self.y = y
         pass
